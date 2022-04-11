c066ce77acff414cebe30ede592b941477ab777a
goateejimmy <47892327+goateejimmy@users.noreply.github.com>
2022-04-11 16:50:17 +0800

    Update myModels.py

--- myModels.py
+++ myModels.py
@@ -19,7 +19,7 @@ a.fc = nn.Linear(in_features, out_features=10)
 class myLeNet(nn.Module):
     def __init__(self, num_out):
         super(myLeNet, self).__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(3,6,kernel_size=5, stride=1),
+        self.conv1 = nn.Sequential(nn.Conv2d(3,6,kernel_size=5, stride=1), #32 32 6
                              nn.ReLU(),
                              nn.MaxPool2d(kernel_size=2, stride=2),
                              )
@@ -73,10 +73,19 @@ class myResnet(nn.Module):
     def __init__(self, in_channels=3, num_out=10):
         super(myResnet, self).__init__()
         
-        self.stem_conv = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=3, padding=1)
+        self.stem_conv = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7, padding=3)
         self.layer1 = residual_block(in_channels=64)
-        self.cnn1 = nn.Sequential(nn.Conv2d(in_channels=64, out_channels=128),
-                                nn.MaxPool2d(),)
+        self.layer2 = residual_block(in_channels=128)
+        self.layer3 = residual_block(in_channels=256)
+        self.expand1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.expand2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.cnn1 = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=128,kernel_size=3, padding=1),
+                                nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc1 = nn.Sequential(nn.Linear(2048, 1024), nn.ReLU())
+        self.fc2 = nn.Sequential(nn.Linear(1024,500), nn.ReLU())
+        self.fc2 = nn.Sequential(nn.Linear(500,84), nn.ReLU())
+        self.fc3 = nn.Linear(84,num_out)
+        
         
 
         ## TO DO ##
@@ -95,6 +104,23 @@ class myResnet(nn.Module):
         # print(x.shape)
         x = self.stem_conv(x)
         x = self.layer1(x)
+        x = self.layer1(x)
+        x = self.layer1(x)
+        x = self.layer1(x)
+        x = self.expand1(x)
+        x = self.layer2(x)
+        x = self.layer2(x)
+        x = self.layer2(x)
+        x =self.expand2(x)
+        x = self.layer3(x)
+        x = self.layer3(x)
+        x = self.layer3(x)
         x = self.cnn1(x)
+
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        out = x
+        return out
 
         pass
